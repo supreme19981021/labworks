3b722da60ffcabea1bcf4d1579310dfb12bc7277
supreme19981021 <1789264231@qq.com>
2018-11-17 09:27:42 +0800

    add the chapt4 demo

---
+++ chapt4/4_04/example4_04_Circle.cpp
@@ -0,0 +1,13 @@
+#include "example4_04_Circle.h"
+double Circle::Area()
+{
+	return PI * Radius*Radius;
+}
+double Circle::Circumference()
+{
+	return 2 * PI*Radius;
+}
+double Circle::GetRadius() const
+{
+	return Radius;
+}
